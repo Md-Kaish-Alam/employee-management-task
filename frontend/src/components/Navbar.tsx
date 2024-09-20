194c4e84100b7606120d0de51a4e78773affdfad
Md-Kaish-Alam <alamkaishg1511@gmail.com>
2024-09-20 23:20:17 +0530

    feat: home page completed.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -18,7 +18,7 @@ import { ModeToggle } from "./mode-toggle";
 export const Navbar = () => {
   const { user, logout } = useContext(AuthContext);
   return (
-    <nav className="fixed top-0 w-full bg-gray-200 dark:bg-gray-800">
+    <nav className="sticky top-0 w-full bg-gray-200 dark:bg-gray-800 shadow-md">
       <div className="md:container md:mx-auto py-4 flex items-center justify-between">
         <Logo/>
         <div className="flex items-center justify-center gap-4">
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,37 @@
-const Home = () => {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-semibold">Welcome Admin Panel</h1>
-    </div>
-  )
-}
+import { useTheme } from "@/hooks/useTheme";
+import readingLight from "@/assets/reading.png";
+import readingDark from "@/assets/reading-dark.png";
+import { cn } from "@/lib/utils";
 
-export default Home
+const Home = () => {
+  const { theme } = useTheme();
+  return (
+    <section className="bg-gray-200 dark:bg-gray-800">
+      <div className="flex flex-wrap justify-between p-4">
+        <div className="w-full md:w-1/2 p-4 flex flex-col items-center justify-center text-left font-semibold gap-4">
+          <span
+            className={cn("text-5xl", {
+              "text-blue-800": theme === "light",
+              "text-blue-600": theme === "dark",
+            })}
+          >
+            Comprehensive Employee Management System
+          </span>
+          <p className="text-muted-foreground">
+            that streamlines employee records, enhances communication, and
+            boosts organizational efficiency.
+          </p>
+        </div>
+        <div className="w-full md:w-1/2 p-4 text-center">
+          <img
+            src={theme === "light" ? readingLight : readingDark}
+            alt="reading mode"
+            className="w-full h-full"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
