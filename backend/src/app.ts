8e41716c1c15a4a2e2520f6098b9048a3dbfa90a
Md-Kaish-Alam <alamkaishg1511@gmail.com>
2024-09-22 18:59:59 +0530

    fix: cors origin issues

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,17 @@
 import cors from "cors";
+import express from "express";
 import mongoose from "mongoose";
 import { config } from "dotenv";
-import express, { Request, Response } from "express";
 
 import authRoutes from "./routes/authRoutes";
 import employeeRoutes from "./routes/employeeRoute";
-import { authMiddleware } from "./middleware/authMiddleware";
-
 config();
 
 const app = express();
 
 const corsOptions = {
-  origin: "https://empowerhub-weld.vercel.app", // or '*' for allowing all origins
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  origin: ["https://empowerhub-weld.vercel.app", "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
@@ -25,9 +23,7 @@ mongoose
   .catch((err) => console.error("Failed to connect to MongoDB", err));
 
 // Middleware
-// app.use(cors());
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 app.use(express.json());
 
 // Routes
